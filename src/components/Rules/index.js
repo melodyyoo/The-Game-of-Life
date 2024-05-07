93120291e8fa27fd240aa8debf5ef8c6ef799d7a
Melody Yoo <melodyyoo72@gmail.com>
2024-05-07 14:40:45 -0700

    style: add styling to Rules

--- src/components/Rules/index.js
+++ src/components/Rules/index.js
@@ -1,14 +1,14 @@
 export default function Rules() {
   return (
-    <div style={{marginLeft:'3rem'}}>
-      <div style={{textDecoration:'underline'}}>
+    <div style={{marginLeft:'3rem', display:'flex', flexDirection:'column', justifyContent:'center'}}>
+      <div style={{textDecoration:'underline', color:'white'}}>
         Rules
       </div>
       <ol>
-        <li>Any live cell with fewer than two live neighbors dies, as if by underpopulation.</li>
-        <li>Any live cell with two or three live neighbors lives on to the next generation.</li>
-        <li>Any live cell with more than three live neighbors dies, as if by overpopulation.</li>
-        <li>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
+        <li style={{color:'white', marginBottom:'10px'}}>Any live cell with fewer than two live neighbors dies, as if by underpopulation.</li>
+        <li style={{color:'white', marginBottom:'10px'}}>Any live cell with two or three live neighbors lives on to the next generation.</li>
+        <li style={{color:'white', marginBottom:'10px'}}>Any live cell with more than three live neighbors dies, as if by overpopulation.</li>
+        <li style={{color:'white', marginBottom:'10px'}}>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
       </ol>
     </div>
   );
